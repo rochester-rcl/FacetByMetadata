1c39234aa9925f29cf673a35813cb2ea064b4fd8
jromphf <jromphf@library.rochester.edu>
2019-07-15 13:16:36 -0400

    works with multi-select for type

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -5,19 +5,37 @@ class FacetByMetadata_IndexController extends Omeka_Controller_AbstractActionCon
     public function indexAction()
     {
         $elementIds = $this->getParam('elements');
+        $itemTypes = $this->getParam('itemtypes');
         $itemId = $this->getParam('item_id');
         $db = $this->_helper->db;
         $item = $db->getTable('Item')->find($itemId);
         $advanced = array();
-        foreach($elementIds as $elementId) {
-            $element = $db->getTable('Element')->find($elementId);
-            $term = metadata($item, array($element->getElementSet()->name, $element->name), array('no_filter' => true));
-            if(!empty($term)) {
-                $advanced[] = array('element_id' => $elementId, 'terms' => $term, 'type' => 'is exactly');
+        $e = $db->getTable('Element')->findByElementSetNameAndElementName('Dublin Core', 'Type');
+        if (count($elementIds) > 0) {
+            foreach ($elementIds as $elementId) {
+                $element = $db->getTable('Element')->find($elementId);
+                if (count($itemTypes) > 0) {
+                    foreach ($itemTypes as $itemType) {
+                        if ($element) {
+                            $term = metadata($item, array($element->getElementSet()->name, $element->name), array('no_filter' => true));
+                            if (!empty($term)) {
+                                $advanced[] = array('element_id' => $elementId, 'terms' => $term, 'type' => 'is exactly', 'joiner' => 'or');
+                            }
+                        }
+                        $advanced[] = array('element_id' => $e->id, 'terms' => $itemType, 'type' => 'is exactly');
+                    }
+                } else {
+                    if ($element) {
+                        $term = metadata($item, array($element->getElementSet()->name, $element->name), array('no_filter' => true));
+                        if (!empty($term)) {
+                            $advanced[] = array('element_id' => $elementId, 'terms' => $term, 'type' => 'is exactly', 'joiner' => 'and');
+                        }
+                    }
+                }
             }
         }
         $paramArray = array('search' => '', 'advanced' => $advanced);
         $params = http_build_query($paramArray);
-        $this->_helper->redirector->gotoUrl('items/browse?' . $params );
+        $this->_helper->redirector->gotoUrl('items/browse?' . $params);
     }
 }
--- libraries/Facet_By_Metadata_Form.php
+++ libraries/Facet_By_Metadata_Form.php
@@ -4,19 +4,45 @@ class Facet_By_Metadata_Form extends Omeka_Form
 {
     protected $item;
 
+    public function getItemTypes()
+    {
+        $db = get_db();
+        $sql = $db->query("SELECT DISTINCT id, name FROM `{$db->prefix}item_types`");
+        $results = $sql->fetchAll();
+        $itemTypes = array();
+        foreach ($results as $result) {
+            $itemTypes[$result['id']] = $result['name'];
+        }
+        return $itemTypes;
+    }
+
     public function init()
     {
         $this->setAction(url('facet-by-metadata'));
         $elTable = get_db()->getTable('Element');
         $elements = array();
         $elementIds = json_decode(get_option('facet_by_metadata_elements'), true);
-        foreach($elementIds as $elementId)
-        {
+        $terms = get_db()->getTable('SimpleVocabTerm');
+        $types = array();
+        foreach ($elementIds as $elementId) {
             $element = $elTable->find($elementId);
-            $elementValue = metadata($this->item, array($element->getElementSet()->name, $element->name), array('no_escape' => true, 'no_filter' => true));
-            if (!empty($elementValue)) {
-                $elements[$elementId] = metadata($element, 'name') . ': ' . $elementValue;
+            if ($element->name === 'Type') {
+                foreach (explode("\n", $terms->findByElementId($elementId)->terms) as $t) {
+                    $types[$t] = $t;
+                }
+            } else {
+                $elementValue = metadata($this->item, array($element->getElementSet()->name, $element->name), array('no_escape' => true, 'no_filter' => true));
+                if (!empty($elementValue) || $elementValue !== NULL) {
+                    $elements[$elementId] = metadata($element, 'name') . ': ' . $elementValue;
+                }
             }
+        }
+
+        if (count($types) > 0) {
+            $multiselect = new Zend_Form_Element_Multiselect('itemtypes');
+            $multiselect->setMultiOptions($types);
+            $multiselect->setLabel('Search by Type');
+            $this->addElement($multiselect);
         }
         $checkboxes = new Zend_Form_Element_MultiCheckbox('elements');
         $checkboxes->setMultiOptions($elements);
